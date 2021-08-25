bc1b908823e7d9965984f958f5b811bcf5cc84ac
Ivan Mikec <mikecivan0@gmail.com>
2021-08-26 01:19:10 +0200

    Gotov projekt

---
+++ LjubavniKalkulator/src/main/java/hr/edunova/ljubavnikalkulator/Start.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.ljubavnikalkulator;
+
+/**
+ *
+ * @author dell
+ */
+public class Start {
+    
+    public static void main(String[] args) {
+        new Prozor().setVisible(true);
+    }
+    
+}
